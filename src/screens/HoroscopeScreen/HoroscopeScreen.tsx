c173926c1813b6470e58149c1a11f4a153786668
lngka <foxlead@gmail.com>
2019-06-23 12:42:56 +0200

    horoscope screen demo

--- src/screens/HoroscopeScreen/HoroscopeScreen.tsx
+++ src/screens/HoroscopeScreen/HoroscopeScreen.tsx
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 class HoroscopeScreen extends Component {
     render () {
         return (
-            <View>
-                <Text>HoroscopeScreen</Text>
+            <View style={styles.container}>
+                <View style={styles.chartArea}>
+                    <Text>Chart goes here</Text>
+                </View>
+                <View style={styles.horoscropeArea}>
+                    <Text>Horoscope goes here</Text>
+                </View>
             </View>
         )
     };
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "grey",
+        height: "100%",
+        flexDirection: "column"
+    },
+    chartArea: {
+        flex: 1,
+        backgroundColor: "#ffcccc"
+    },
+    horoscropeArea: {
+        flex: 2,
+        backgroundColor: "#ccffff"
+    }
+});
 
 export default HoroscopeScreen;
